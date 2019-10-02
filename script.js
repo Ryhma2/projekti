855ccc9739661f5eb988fcb5283ba3a06c3c9dfc
unknown <you@example.com>
2019-10-02 10:21:12 +0300

    script

---
+++ script.js
@@ -0,0 +1,32 @@
+(function() {
+  mapboxgl.accessToken = 'pk.eyJ1Ijoib2JiMTIzIiwiYSI6ImNqdHBmY3U2ejAzczc0NGw1dmV0cmtkemcifQ.NYv9m8AnsqVHTop8Ay_k6w';
+  var map = new mapboxgl.Map({
+  container: 'map',
+  center:[25,60.2],
+  zoom:9,
+  style: 'mapbox://styles/mapbox/streets-v11'
+  });
+  map.addControl(new mapboxgl.NavigationControl());
+  axios.get('https://unicafe.fi/wp-json/swiss/v1/restaurants?lang=fi')
+  .then(function (response) {
+    // handle success
+
+    const result = response.data.filter(data => data.address != null);
+  for (item of result) {
+    console.log(item)
+      axios.get("https://api.digitransit.fi/geocoding/v1/search?text="+ item.address).then(function(response) {
+      }).catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+  }
+
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+}());
